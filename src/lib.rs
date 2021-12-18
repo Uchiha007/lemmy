7dbe7d29ead22e944567cebbf71095d8dea419ff
Aode (lion) <asonix@asonix.dog>
2021-12-17 22:27:33 -0600

    Add console-subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,34 @@ pub mod code_migrations;
 pub mod root_span_builder;
 pub mod scheduled_tasks;
 
+use console_subscriber::ConsoleLayer;
 use lemmy_utils::LemmyError;
 use tracing::subscriber::set_global_default;
 use tracing_error::ErrorLayer;
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{filter::Targets, layer::SubscriberExt, Layer, Registry};
 
 pub fn init_tracing() -> Result<(), LemmyError> {
   LogTracer::init()?;
 
-  let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-  let format_layer = tracing_subscriber::fmt::layer();
+  let log_description = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".into());
+
+  let targets = log_description
+    .trim()
+    .trim_matches('"')
+    .parse::<Targets>()?;
+
+  let format_layer = tracing_subscriber::fmt::layer().with_filter(targets);
+
+  let console_layer = ConsoleLayer::builder()
+    .with_default_env()
+    .server_addr(([0, 0, 0, 0], 6669))
+    .spawn();
 
   let subscriber = Registry::default()
-    .with(env_filter)
     .with(format_layer)
-    .with(ErrorLayer::default());
+    .with(ErrorLayer::default())
+    .with(console_layer);
 
   set_global_default(subscriber)?;
 
